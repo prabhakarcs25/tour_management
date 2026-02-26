3f9d0e75fa06abc9c59a9e0c3c71e625c02092fd
PRABHAKAR S SINGH <prabhakarsingh63915@gmail.com>
2026-02-26 13:08:26 +0530

    After hosting onrender

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,6 @@
-export const BASE_URL='http://localhost:4000/api/v1'
+// export const BASE_URL='http://localhost:4000/api/v1'
 
-// export const BASE_URL='https://tour-management-a2x6.onrender.com'
+// export const BASE_URL='https://tour-management-3083.onrender.com'
+// export const BASE_URL='http://localhost:4000/api/v1'
+
+export const BASE_URL='https://tour-management-3083.onrender.com'
